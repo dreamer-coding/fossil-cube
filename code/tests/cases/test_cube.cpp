d8a0e648462e15d3bcdc97f4cd4811b65aa46fb0
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-08-11 21:48:18 -0600

    add guts

---
+++ code/tests/cases/test_cube.cpp
@@ -0,0 +1,55 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Project: Fossil Logic
+ *
+ * This file is part of the Fossil Logic project, which aims to develop high-
+ * performance, cross-platform applications and libraries. The code contained
+ * herein is subject to the terms and conditions defined in the project license.
+ *
+ * Author: Michael Gene Brockus (Dreamer)
+ *
+ * Copyright (C) 2024 Fossil Logic. All rights reserved.
+ * -----------------------------------------------------------------------------
+ */
+#include <fossil/pizza/framework.h>
+#include "fossil/cube/framework.h"
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test Utilities
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// Setup steps for things like test fixtures and
+// mock objects are set here.
+// * * * * * * * * * * * * * * * * * * * * * * * *
+
+FOSSIL_TEST_SUITE(cpp_cube_fixture);
+
+FOSSIL_SETUP(cpp_cube_fixture) {
+    // Setup the test fixture
+}
+
+FOSSIL_TEARDOWN(cpp_cube_fixture) {
+    // Teardown the test fixture
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test Cases
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// The test cases below are provided as samples, inspired
+// by the Meson build system's approach of using test cases
+// as samples for library usage.
+// * * * * * * * * * * * * * * * * * * * * * * * *
+
+FOSSIL_TEST_CASE(cpp_test_blaink) {
+    ASSUME_ITS_TRUE(1);
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test Pool
+// * * * * * * * * * * * * * * * * * * * * * * * *
+FOSSIL_TEST_GROUP(cpp_cube_tests) {    
+    // C++ Wrapper Tests
+    FOSSIL_TEST_ADD(cpp_cube_fixture, cpp_test_blaink);
+
+    FOSSIL_TEST_REGISTER(cpp_cube_fixture);
+} // end of tests
